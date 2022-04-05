60c0ed363e2c2ffd5684cbd33134cfdcf91097e5
DESKTOP-KOHNKEM\Евгений <piualion@mail.ru>
2022-04-05 11:31:49 +0700

    add file's base_page and main_page, add methods

---
+++ pages/base_page.py
@@ -0,0 +1,10 @@
+#base_page
+
+class BasePage():
+    
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+
+    def open(self):
+        self.browser.get(self.url)

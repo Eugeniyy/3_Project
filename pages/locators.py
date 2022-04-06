377e8101fb68ff7b54c22c47c31f06f7a488f3b9
DESKTOP-KOHNKEM\Евгений <piualion@mail.ru>
2022-04-06 13:17:00 +0700

    add tests for login page

---
+++ pages/locators.py
@@ -0,0 +1,11 @@
+#locators.py - модуль с селекторами
+
+from selenium.webdriver.common.by import By
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form1")
+    REGISTRATION_FORM = (By.CSS_SELECTOR, "#register_form1")

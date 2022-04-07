b423f10de17b36bea28c7d46127086711362b4e7
DESKTOP-KOHNKEM\Евгений <piualion@mail.ru>
2022-04-07 13:18:15 +0700

    add test for product_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,4 @@
-#test_main_page.py - Тестируем главную страницу
+#test_main_page.py - тестируем главную страницу
 
 from pages.login_page import LoginPage
 from pages.main_page import MainPage
@@ -11,5 +11,6 @@ def test_guest_can_go_to_login_page(browser):
     page.should_be_login_link()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+
     
 #Пустая строка!
